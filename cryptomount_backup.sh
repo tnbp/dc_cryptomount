04dc21d042b35017cedd70be4a53f7da9443f81b
tnbp <ian@d.atacube.de>
2020-11-14 10:27:53 +0100

    Update cryptomount_backup.sh

--- cryptomount_backup.sh
+++ cryptomount_backup.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-echo "Enabling backup drives..."
-/opt/datacubic_relaycontrol.py -c 1
+if [[ "$1" != "--force" ]]; then
+        echo -e "\e[1m[\e[95m*\e[39m]\e[0m Enabling backup drives..."
+        /opt/datacubic_relaycontrol.py -c 1
+else
+        echo -e "\e[1m[\e[93m!\e[39m]\e[0m Forcefully skipping power-on..."
+fi
 
 if [[ "$?" -ne 0 ]]; then
-        echo "Could not enable backup drives--exiting!"
+        echo -e "\e[1m[\e[91m-\e[39m]\e[0m Could not enable backup drives--exiting!"
         exit 1
 fi
 
-echo "Success! Allowing 10 seconds for devices to get ready..."
+echo -e "\e[1m[\e[92m+\e[39m]\e[0m Success! Allowing 10 seconds for devices to get ready..."
 
 sleep 10
-
-# BEGIN PLAGIARISM
 
 unset PASSWORD
 unset CHARCOUNT
 
-echo -n "Enter password: "
+echo -ne "\e[1m[\e[96m?\e[39m]\e[0m Enter password: "
 
 stty -echo
 
@@ -47,43 +49,41 @@ done
 stty echo
 echo
 
-# END PLAGIARISM
-
 echo -n $PASSWORD | cryptsetup open /dev/disk/by-partlabel/CRYPTO_BACKUP_A CRYPTO_BACKUP_A -d -
 if [ -h /dev/mapper/CRYPTO_BACKUP_A ]; then
-        echo "Successfully unlocked CRYPTO_BACKUP_A!"
+        echo -e "\e[1m[\e[92m+\e[39m]\e[0m Successfully unlocked CRYPTO_BACKUP_A!"
 else
-        echo "Could not unlock CRYPTO_BACKUP_A!"
+        echo -e "\e[1m[\e[91m-\e[39m]\e[0m Could not unlock CRYPTO_BACKUP_A!"
 fi
 echo -n $PASSWORD | cryptsetup open /dev/disk/by-partlabel/CRYPTO_BACKUP_B CRYPTO_BACKUP_B -d -
 if [ -h /dev/mapper/CRYPTO_BACKUP_B ]; then
-        echo "Successfully unlocked CRYPTO_BACKUP_B!"
+        echo -e "\e[1m[\e[92m+\e[39m]\e[0m Successfully unlocked CRYPTO_BACKUP_B!"
 else
-        echo "Could not unlock CRYPTO_BACKUP_B!"
+        echo -e "\e[1m[\e[91m-\e[39m]\e[0m Could not unlock CRYPTO_BACKUP_B!"
 fi
 
-mount -L CRYPTO_BACKUP_A || echo "Could not mount CRYPTO_BACKUP_A!"
-mount -L CRYPTO_BACKUP_B || echo "Could not mount CRYPTO_BACKUP_B!"
+mount -L CRYPTO_BACKUP_A || echo -e "\e[1m[\e[91m-\e[39m]\e[0m Could not mount CRYPTO_BACKUP_A!"
+mount -L CRYPTO_BACKUP_B || echo -e "\e[1m[\e[91m-\e[39m]\e[0m Could not mount CRYPTO_BACKUP_B!"
 
 BACKUP_ERROR=0
 
 if [ $(lsblk | grep /mnt/.backup_a | wc -l) -eq "0" ]; then
-        echo "ERROR: Asked to mount CRYPTO_BACKUP_A, but it is not mounted!"
+        echo -e "\e[1m[\e[91m-\e[39m]\e[0m Asked to mount CRYPTO_BACKUP_A, but it is not mounted!"
         BACKUP_ERROR=1
 fi
 if [ $(lsblk | grep /mnt/.backup_b | wc -l) -eq "0" ]; then
-        echo "ERROR: Asked to mount CRYPTO_BACKUP_B, but it is not mounted!"
+        echo -e "\e[1m[\e[91m-\e[39m]\e[0m Asked to mount CRYPTO_BACKUP_B, but it is not mounted!"
         BACKUP_ERROR=1
 fi
 
 if [ "$BACKUP_ERROR" -ne 1 ]; then
-        echo "Merging CRYPTO_BACKUP_* ..."
+        echo -e "\e[1m[\e[95m*\e[39m]\e[0m Merging CRYPTO_BACKUP_* ..."
         mergerfs -o use_ino,cache.files=off,dropcacheonclose=true,allow_other,category.create=mfs /mnt/.backup_\* /mnt/backup
         if [ $(mount | grep /mnt/backup | wc -l) -eq "0" ]; then
-                echo "ERROR: Could not merge CRYPTO_BACKUP_*!"
+                echo -e "\e[1m[\e[91m-\e[39m]\e[0m Could not merge CRYPTO_BACKUP_*!"
         else
-                echo "Merged CRYPTO_BACKUP_* to /mnt/backup."
+                echo -e "\e[1m[\e[92m+\e[39m]\e[0m Merged CRYPTO_BACKUP_* to /mnt/backup."
         fi
 else
-        echo "CRYPTO_BACKUP_* volumes do not seem ready--not merging!"
+        echo -e "\e[1m[\e[91m-\e[39m]\e[0m CRYPTO_BACKUP_* volumes do not seem ready--not merging!"
 fi
