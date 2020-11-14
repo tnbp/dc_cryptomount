0c3279b16447ae20a770fb7093b3581511f76b7b
tnbp <ian@d.atacube.de>
2020-11-14 10:29:21 +0100

    Update cryptumount_extbckp.sh

--- cryptumount_extbckp.sh
+++ cryptumount_extbckp.sh
@@ -9,45 +9,45 @@ isMounted () {
 STATUS=0
 
 if isMounted "/mnt/extbckp"; then
-        echo "Umounting /mnt/extbckp ..."
+        echo -e "\e[1m[\e[95m*\e[39m]\e[0m Umounting /mnt/extbckp ..."
         umount /mnt/extbckp
         STATUS="$?"
 fi
 
 if [ "$STATUS" -ne 0 ]; then
-        echo "Could not umount /mnt/extbckp -- exiting!"
+        echo -e "\e[1m[\e[91m-\e[39m]\e[0m Could not umount /mnt/extbckp -- exiting!"
         exit 1
 fi
 
 for CUR_DRIVE in $DRIVES; do
         if isMounted "/mnt/.extbckp_$CUR_DRIVE"; then
-                echo "Umounting /mnt/.extbckp_$CUR_DRIVE ..."
+                echo -e "\e[1m[\e[95m*\e[39m]\e[0m Umounting /mnt/.extbckp_$CUR_DRIVE ..."
                 umount /mnt/.extbckp_$CUR_DRIVE
                 STATUS="$?"
         fi
 
         if [ "$STATUS" -ne 0 ]; then
-                echo "Could not umount /mnt/.backup_a -- exiting!"
+                echo -e "\e[1m[\e[91m-\e[39m]\e[0m Could not umount /mnt/.backup_a -- exiting!"
                 exit 1
         fi
 
 
         if [ -h /dev/mapper/CRYPTO_EXTBCKP_$CUR_DRIVE ]; then
-                echo "Closing crypto volume CRYPTO_EXTBCKP_$CUR_DRIVE ..."
+                echo -e "\e[1m[\e[95m*\e[39m]\e[0m Closing crypto volume CRYPTO_EXTBCKP_$CUR_DRIVE ..."
                 cryptsetup close CRYPTO_EXTBCKP_$CUR_DRIVE
                 STATUS="$?"
         fi
 
         if [ "$STATUS" -ne 0 ]; then
-                echo "Could not close crypto volume CRYPTO_EXTBCKP_$CUR_DRIVE -- exiting!"
+                echo -e "\e[1m[\e[91m-\e[39m]\e[0m Could not close crypto volume CRYPTO_EXTBCKP_$CUR_DRIVE -- exiting!"
                 exit 1
         fi
 
-        echo "Successfully crypto-umounted CRYPTO_BACKUP_$CUR_DRIVE!"
+        echo -e "\e[1m[\e[92m+\e[39m]\e[0m Successfully crypto-umounted CRYPTO_BACKUP_$CUR_DRIVE!"
         sleep 2
-        echo "Detaching..."
+        echo -e "\e[1m[\e[95m*\e[39m]\e[0m Detaching..."
         udisksctl power-off -b /dev/disk/by-partlabel/CRYPTO_EXTBCKP_$CUR_DRIVE
 done
 
-echo "Syncing..."
+echo -e "\e[1m[\e[95m*\e[39m]\e[0m Syncing..."
 sync
